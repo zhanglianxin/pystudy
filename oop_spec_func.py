5f0ccefce5bde1e354cfcdcd09d1c6f181f8cddb
zhanglianxin <zhanglianxin1993@163.com>
2016-12-31 00:08:20 +0800

    __call__

--- oop_spec_func.py
+++ oop_spec_func.py
@@ -96,10 +96,50 @@ class Student(object):
 # 当访问不存在的属性时，解释器会试图调用 __getattr__(self, 'score') 
 #  来尝试获得属性。只有在未找到属性的情况下才会调用 __getattr_() 。
 s = Student()
-print s.name
-print s.score
-print s.age()
-print s.abc # 访问其它不存在的属性返回 None
+print s.name # Michael
+print s.score # 99
+print s.age() # 25
+# print s.abc # 访问其它不存在的属性返回 None
 # 这实际上就把一个类的所有属性和方法调用全部动态化处理了，不需要任何特殊手段。
 # 这种完全动态调用的特性有什么实际作用呢？ 可以针对完全动态的情况作调用。
 
+# 利用完全动态的 __getattr__ 可以写一个链式调用
+class Chain(object):
+    """docstring for Chain"""
+    def __init__(self, path = '/users'):
+        self._path = path
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+    def __str__(self):
+        return self._path
+
+    def users(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+print Chain().status.user.timeline.list # /status/user/timeline/list
+print Chain().users('Michael').repos # /users/Michael/repos
+
+# __call__ 直接对实例进行调用
+class Student(object):
+    """docstring for Student"""
+    def __init__(self, name = ''):
+        self.name = name
+    def __call__(self):
+        print('My name is %s' % self.name)
+
+s = Student('Michael')
+s() # My name is Michael
+# __call__() 还可以定义参数。
+#  对实例进行直接调用就好比对一个函数进行调用一样，可以把对象看成函数，
+#  把函数看成对象，因为这两者之间本来就没啥根本的区别。
+#  如果把对象看成函数，那么函数本身其实也可以在运行期动态创建出来，
+#  因为类的实例都是运行期创建出来的，这么一来就模糊了对象和函数的界限。
+
+# 怎么判断一个变量是对象还是函数？
+# 需要判断一个对象是否能被调用，能被调用的对象就是一个 Callable 对象。
+print callable(Student()) # True
+print callable(max) # True
+print callable([1, 2, 3]) # False
+print callable(None) # False
+print callable('string') # False
+# 通过 callable() 函数，就可以判断一个对象是否是可调用对象
